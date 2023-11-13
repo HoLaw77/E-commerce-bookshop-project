ccb22c6300a75ff8f0e588ccbbed8cbec93b08d0
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-11-13 22:27:06 +0000

    update Order models

--- order/models.py
+++ order/models.py
@@ -1,8 +1,20 @@
 from django.db import models
-
+from django.address import AddressField
+from django.countries import CountryField
 # Create your models here
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=64, null=False, editable=False)
-    full
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    address = AddressField()
+    countries = CountryField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def _generate_order_number(self):
+        
+        return uuid.uuid4().hex.upper()
+
+    def __str__(self):
+        return self.order_number
+
