a14a9a9ae296b438da23fe7962a4430a9cfd7033
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-11-17 21:59:13 +0000

    set up admin site register for models

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Book_Interest, Profile
 # Register your models here.
+
+admin.site.register(Book_Interest)
+admin.site.register(Profile)
--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Language, Category, Product, ProductImage
 # Register your models here.
+
+admin.site.register(Language)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+
+
