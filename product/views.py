711c0c14cd9d9aafa8e5f3f883fdc446754be8a2
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-11-21 09:11:07 +0000

    add search function and add if statement to template

--- product/views.py
+++ product/views.py
@@ -1,17 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Product, ProductImage, Language, Category 
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
 def show_book(request):
 
     books = Product.objects.all()
+    query = None
     language = Language.objects.all()
     images = ProductImage.objects.all()
+    
+    if request.GET:   
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter searching keyword")
+                return redirect(reverse('book'))
+                
+            queries = Q(name__icontains=query) | Q(author__icontains=query)| Q(isbn__icontains=query)| Q(publisher__icontains=query)
+            books = books.filter(queries)
+
     context = {
         'books': books,
         'images': images,
         'language': language,
+        'search': query,
     }
     return render(request, 'book/book.html', context)
 
