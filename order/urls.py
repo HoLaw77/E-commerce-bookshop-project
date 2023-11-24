2db0db867e2f45c61ee7940e993947cc30621adf
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-11-24 18:07:26 +0000

    build the form for making order

--- order/urls.py
+++ order/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('order', views.show_order, name='order'),
+    path('order/<product_id>/', views.add_order, name='add_order'),
 ]
--- order/views.py
+++ order/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 def show_order(request):
 
     return render (request, 'order/order.html')
+
+def add_order(request, product_id):
+    """Add order to cart"""
+
+    book = get_object_or_404(Product, pk=product_id)
+    quantity = int(request.POST.get('quantity'))
+    order = request.session.get('order', {})
+
+    if product_id in list(order.keys()):
+        order[product_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {order[product_id]}')
+    else:
+        order[product_id] = quantity
+        messages.success(request, f'Added {product.name} to cart')
+
+    request.session['order'] = order
+    print(order)
+    return redirect(redirect_url)
