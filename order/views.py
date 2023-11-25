5dba42f427b09239ea7a17614a1f7af643884b84
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-11-25 22:35:55 +0000

    try to debug book_detail add to cart

--- order/views.py
+++ order/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render, redirect
-
+from .models import Order, OrderDetail
 # Create your views here.
 def show_order(request):
 
     return render (request, 'order/order.html')
 
 def add_order(request, book_id):
-    """Add order to cart"""
-
+    """Add individual book to cart"""
+    product = get_object_or_404(Product, id=book_id)
     redirect_url = request.POST.GET('redirect_url')
-    order = Order.objects.create()
+    order = request.session.get('order', {})
 
     if book_id in list(order.keys()):
         order[book_id].create()
         messages.success(request, f'Added {product.name} to cart')
     else: 
-        return redirect(book_detail)
+        return reverse(book_detail)
     request.session['order'] = order
     print(order)
+    return redirect(redirect_url)
+
     
-    return redirect(book_detail)
