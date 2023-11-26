544a8326027ec1b13b3527f62f8150e95212b852
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-11-26 16:49:52 +0000

    create contexts.py

---
+++ order/contexts.py
@@ -0,0 +1,11 @@
+def order_contents(request):
+    order_items = []
+    total = 0
+    product_count = 0 
+    context = {
+        order_items
+        total
+        product_count
+    }
+
+    return context
--- order/views.py
+++ order/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import Order, OrderDetail
+from django.shortcuts import get_object_or_404
+from product.models import Product
+
 # Create your views here.
 def show_order(request):
 
     return render (request, 'order/order.html')
 
-def add_order(request, book_id):
+def add_order(request, books_id):
     """Add individual book to cart"""
-    product = get_object_or_404(Product, id=book_id)
-    redirect_url = request.POST.GET('redirect_url')
+    product = get_object_or_404(Product, id=books_id)
+    
     order = request.session.get('order', {})
 
-    if book_id in list(order.keys()):
-        order[book_id].create()
+    if books_id in list(order.keys()):
+        order += order[books_id].create(customer=user)
         messages.success(request, f'Added {product.name} to cart')
-    else: 
-        return reverse(book_detail)
     request.session['order'] = order
     print(order)
-    return redirect(redirect_url)
+    
+    context= {
+        "order": order
+    }
+    return render (request, "order/order.html", context )
 
     
