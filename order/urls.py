9ee38a08d6e1660d7727f30a8e413550613a6342
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-12-03 17:13:44 +0000

    add books id in the url

--- order/urls.py
+++ order/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('order', views.show_order, name='order'),
     path('add_order/<int:books_id>', views.add_order, name='add_order'),
     path('adjust_order', views.adjust_order, name='adjust_order'),
-    path('remove_order', views.remove_order, name='remove_order'),
+    path('remove_order/<int:books_id>', views.remove_order, name='remove_order'),
 
 ]
