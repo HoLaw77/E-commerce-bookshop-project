e87b5775efcfbd3939bf415fe2f14cb4ea5b3164
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-12-05 22:13:58 +0000

    render form in html

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,23 +1,24 @@
 from django import forms
 from .models import Order
 
-class ConfirmOrder(forms.Modelform):
+class ConfirmOrder(forms.ModelForm):
     class Meta:
         model = Order
         fields = ("full_name", "countries", 
-    "email", "phone_number", "address1", "address2", "postcode", 
-    "country")
+    "email", "phone_number", "address1", "address2", "postcode")
 
 def __init__(self, *args, **kwargs):
     """Add placeholder for form fields"""
 
     super().__init__(*args, **kwargs)
-    placeholder(
+    placeholders = {
+
         "full_name": "Full Name",  
         "email": "Your Email", 
         "phone_number": "Phone Number", 
         "address1": "Address Line 1", 
         "address2": "Address Line 2", 
-        "postcode": "Postdode", 
+        "postcode": "Postcode", 
         "countries": "Country",
-    )
+    }
+    
