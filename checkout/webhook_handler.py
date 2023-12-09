0229a4527f7ac20dbf89b24dc5fea828c87edb28
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-12-09 00:03:59 +0000

    Set webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse,
+from django.http import HttpResponse
 
 class StripeWH_Handler:
     """Handle Stripe Webhook"""
@@ -9,6 +9,18 @@ class StripeWH_Handler:
     def handle_event(self, event):
         """Handle unexpected Event"""
 
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}', status = 200
+        )
+
+    def handle_payment_intent_success(self, event):
+        """Handle successful payment intent event webhook from Stripe"""
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}', status = 200
+        )
+
+    def handle_payment_intent_fail(self, event):
+        """Handle fail payment intent event webhook from Stripe"""
         return HttpResponse(
             content=f'Webhook received: {event["type"]}', status = 200
         )
